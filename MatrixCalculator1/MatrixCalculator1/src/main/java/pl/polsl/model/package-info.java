d724958dbf83e8ae89a088a005141aaf0a2f59cd
Mikołaj Mach <mach.mikolaj@wp.pl>
2024-11-28 12:01:29 +0100

    Matrix Calculator
    Normal calc, UI, UnitTests, Record, etc.

--- MatrixCalculator1/MatrixCalculator1/src/main/java/pl/polsl/model/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Model package for the matrix calculator application.
- * This package contains the methods that perform the calculating operations as also an exception handler
- * 
- * @version 1.0
- * 
- * @author MIKOLAJ MACH
- */
-package pl.polsl.model;
--- MatrixCalculator1/MatrixCalculator1/src/main/java/pl/polsl/view/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * pl.polsl.view package for the matrix calculator application.
- * 
- * @version 1.0
- * 
- * @author MIKOLAJ MACH
- */
-package pl.polsl.view;
